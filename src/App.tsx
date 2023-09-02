b69924ed708d616548308162c70cba09c4f53e57
Laravel007 <zikriismal42@gmail.com>
2023-09-03 00:27:01 +0700

    change wording to coming-soon

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <>
       <h1 className="font-semibold">Color extraction</h1>
-      <div className="mt-10">render somethin</div>
+      <div className="mt-10">coming soon :)</div>
     </>
   );
 }
